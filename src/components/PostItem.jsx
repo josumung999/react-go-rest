3bd5fe92ecb32a7465eea33fc13d488babb8bd16
Josué Munganga <jmunganga039@gmail.com>
2022-01-12 11:53:16 +0200

    Fixed a margin issue

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -20,19 +20,19 @@ const PostItem = () => {
   }, [id])
   return (
     <Fragment>
-      <div className="container my-6">
+      <div className="container my-6" style={{paddingTop: "24px"}}>
         {isLoaded ? (
           <Fragment>
             <h2 className="h2">
               {post.data.title}
             </h2>
             <hr className="my-4" />
-            <span className="bg-info">By user: {post.data.user_id}</span>
+            <span className="bg-info mb-4">By user: {post.data.user_id}</span>
             <p className="lead">
               {post.data.body}
             </p>
             <hr className="my-4" />
-            <h2 className="h2 mb-4">Latest Comments</h2>
+            <h3 className="h3 mb-4">Latest Comments</h3>
             <Comments postId={post.data.id} />
           </Fragment>
         ) : (
